b92ea25c047bc974f4095533f59af5b770d1973e
Matheus Rodrigues <135967315+matheuszsh@users.noreply.github.com>
2024-02-13 20:30:24 -0300

    Update construir.c
    renomeando

--- construir.c
+++ construir.c
@@ -4,6 +4,6 @@
 int main(void){
 
 	printf("***|Construindo o Arquivo|***\n");
-	system("gcc main.c encrypt.c decrypt.c -o cipherofcesar");
-	printf("Arquivo cipherofcesar construido.\n\n./cipherofcesar para usar\n");
+	system("gcc main.c encrypt.c decrypt.c -o cipherofcaesar");
+	printf("Arquivo cipherofcaesar construido.\n\n./cipherofcaesar para usar\n");
 }
